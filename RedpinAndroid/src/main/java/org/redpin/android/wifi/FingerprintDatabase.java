fc1184e8d7ba4337b7efe90458dccce3dc004840
githubflim1 <foongfoong123@yahoo.com>
2016-09-23 16:57:10 +0800

    Wifi database code with debug messages

--- RedpinAndroid/src/main/java/org/redpin/android/wifi/FingerprintDatabase.java
+++ RedpinAndroid/src/main/java/org/redpin/android/wifi/FingerprintDatabase.java
@@ -1,5 +1,7 @@
 package org.redpin.android.wifi;
 
+import android.util.Log;
+
 /**
  * Created by Admin on 9/18/2016.
  * To keep wifi fingerprint database
@@ -7,5 +9,36 @@ package org.redpin.android.wifi;
 
 public class FingerprintDatabase {
 
+    String[][] wifiData3points = {
+            {"84:24:8d:3f:a2:e0",
+                    "6-004"},
+            {"84:24:8d:3f:7c:e0",
+                    "6-005"},
+            {"84:24:8d:3f:8a:d1",
+                    "6-010"}
+    };
 
+    public String getLocationByBSSID(String BSSID)
+    {
+        boolean found = false;
+        String retVal = "";
+        for (int i=0; i<3; i++)
+        {
+            if (BSSID == wifiData3points[i][0])
+            {
+                found = true;
+                retVal = wifiData3points[i][1];
+            }
+        }
+
+        if (found)
+        {
+            Log.i("debug", "#######################" + BSSID + retVal);
+            return retVal;
+        }
+        else
+        {
+            return "notFound";
+        }
+    }
 }
--- RedpinAndroid/src/main/java/org/redpin/android/wifi/WifiInformation.java
+++ RedpinAndroid/src/main/java/org/redpin/android/wifi/WifiInformation.java
@@ -39,7 +39,12 @@ public class WifiInformation {
 					+ ", BSSID: "+currentScanList.get(i).BSSID
 					+ ", SSID: "+currentScanList.get(i).SSID
 					+ ", level: "+currentScanList.get(i).level);
+
    	    }
+		FingerprintDatabase testDB = new FingerprintDatabase();
+		String locationString = testDB.getLocationByBSSID("84:24:8d:3f:a2:e0");
+		locationString = testDB.getLocationByBSSID("84:24:8d:3f:7c:e0");
+		locationString = testDB.getLocationByBSSID("84:24:8d:3f:8a:d1");
 		numOfScans++;
 		
 		level[0]=-100f;
