ae607176788e2ded83f50d482d17ee0da6ff05d5
woonjietchong <vertwj@gmail.com>
2016-09-27 17:24:33 +0800

    27-Sep-2016 wj leave comment on what coordinate to mark

--- RedpinAndroid/src/main/java/org/redpin/android/wifi/WifiInformation.java
+++ RedpinAndroid/src/main/java/org/redpin/android/wifi/WifiInformation.java
@@ -4,6 +4,8 @@ import java.util.List;
 import android.net.wifi.ScanResult;
 import android.util.Log;
 
+import org.redpin.android.ui.MapViewActivity;
+
 public class WifiInformation {
 
 	List<ScanResult> currentScanList;
@@ -14,22 +16,20 @@ public class WifiInformation {
 		currentScanList = null;
 	}
 	
-	public void updateInformation(List<ScanResult> wifiScanList)
-	{
-		
+	public void updateInformation(List<ScanResult> wifiScanList) {
+
 		currentScanList = wifiScanList;
 		int j = 0;
 
 		numOfScans++;
 
-		for(int i = 0; i < wifiScanList.size(); i++)
-   	    {
-			Log.i("wj","numOfScans: " + numOfScans
-					+ ", BSSID: "+currentScanList.get(i).BSSID
-					+ ", SSID: "+currentScanList.get(i).SSID
-					+ ", level: "+currentScanList.get(i).level);
+		for (int i = 0; i < wifiScanList.size(); i++) {
+			Log.i("wj", "numOfScans: " + numOfScans
+					+ ", BSSID: " + currentScanList.get(i).BSSID
+					+ ", SSID: " + currentScanList.get(i).SSID
+					+ ", level: " + currentScanList.get(i).level);
 
-   	    }
+		}
 		FingerprintDatabase testDB = new FingerprintDatabase();
 		String locationString = testDB.getLocationByBSSID("84:24:8d:3f:a2:e0");
 		locationString = testDB.getLocationByBSSID("84:24:8d:3f:7c:e0");
@@ -38,8 +38,24 @@ public class WifiInformation {
 
 		//----------------------------------------------------------
 
-		  // Do some processing here
+		// Do some processing here
 
 		//----------------------------------------------------------
+
+		//If decided point
+		//{
+            //Set Location Marker to the point
+
+		// Here is the point of 6-004 = (x = 306,y = 536) in pixels
+		//MapViewActivity.setMarkerLocation(306, 536);
+
+		// Here is the point of 6-005 = (x = 410,y = 346) in pixels
+		//MapViewActivity.setMarkerLocation(410, 346);
+
+		// Here is the point of 6-010 = (x = 880,y = 510) in pixels
+		//MapViewActivity.setMarkerLocation(880, 510);
+
+	    //}
+
 	}
 }
