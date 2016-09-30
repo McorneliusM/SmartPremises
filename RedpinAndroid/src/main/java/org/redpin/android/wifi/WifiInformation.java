ae5d4e8fa4181fb0223559943fd870b13d90bf15
ericlimjc <ericlimjc19@gmail.com>
2016-09-30 15:18:14 +0800

    Add comparator

--- RedpinAndroid/src/main/java/org/redpin/android/wifi/WifiInformation.java
+++ RedpinAndroid/src/main/java/org/redpin/android/wifi/WifiInformation.java
@@ -19,41 +19,80 @@ public class WifiInformation {
 	public void updateInformation(List<ScanResult> wifiScanList) {
 
 		currentScanList = wifiScanList;
+		int currentScanListSize = currentScanList.size();
 		int j = 0;
+
+		// Initialize values for comparator
+		int counterOfBSSIDWithStrongestRSSI = 0;
+		int compareA = currentScanList.get(counterOfBSSIDWithStrongestRSSI).level;
 
 		numOfScans++;
 
 		FingerprintDatabase testDB = new FingerprintDatabase();
 
-		for (int i = 0; i < wifiScanList.size(); i++) {
+		//---------------------------------------------------------------
+		// Comparator
+		//---------------------------------------------------------------
+
+		// Check if size must be at least 2 for a comparison
+		if (currentScanListSize >= 2) {
+
+			// Initialize values to be compared
+			// B is only available in this case, thus it is initialized here.
+			int compareB = currentScanList.get(1).level;
+
+			// Main loop for comparator
+			for (int i = 0; i < (currentScanListSize - 2); i++) {
+
+				// If A is less than or equal to B,
+				// copy value of B into A for next comparison.
+				// else remain the value of A, since it is bigger.
+				// Note: this code works with raw negative values of dB
+				if (compareA <= compareB) {
+					compareA = compareB;
+					counterOfBSSIDWithStrongestRSSI = i + 1;
+				}
+
+				// If there are more data,
+				// update values to be compared in B.
+				if ((i + 2) < currentScanListSize) {
+					compareB = currentScanList.get(i + 2).level;
+				}
+			}
+		}
+
+
+		//---------------------------------------------------------------
+		// Get location from database based on strongest RSSI
+		//---------------------------------------------------------------
+		String locationString = testDB.getLocationByBSSID(currentScanList.get(counterOfBSSIDWithStrongestRSSI).BSSID);
+		if (locationString.equals("6-004")) {
+			MapViewActivity.setMarkerLocation(306, 536);
+		}
+		else if (locationString.equals("6-005"))
+		{
+			MapViewActivity.setMarkerLocation(410, 346);
+		}
+		else if (locationString.equals("6-010"))
+		{
+			MapViewActivity.setMarkerLocation(880, 510);
+		}
+		else
+		{
+			//to prove that it not work
+			//MapViewActivity.setMarkerLocation(555, 222);
+		}
+
+		//---------------------------------------------------------------
+		// Log scan list
+		//---------------------------------------------------------------
+		for (int i = 0; i < currentScanListSize; i++) {
 			Log.i("wj", "numOfScans: " + numOfScans
 					+ ", BSSID: " + currentScanList.get(i).BSSID
 					+ ", SSID: " + currentScanList.get(i).SSID
 					+ ", level: " + currentScanList.get(i).level);
 
-
-			// Problem: locationString need to be array because every numofscan it will return the latest BSSID.
-			String locationString = testDB.getLocationByBSSID(currentScanList.get(i).BSSID);
-			if (locationString.equals("6-004")) {
-				MapViewActivity.setMarkerLocation(306, 536);
-			}
-			else if (locationString.equals("6-005"))
-			{
-				MapViewActivity.setMarkerLocation(410, 346);
-			}
-			else if (locationString.equals("6-010"))
-			{
-				MapViewActivity.setMarkerLocation(880, 510);
-			}
-			else
-			{
-				//to prove that it not work
-				//MapViewActivity.setMarkerLocation(555, 222);
-			}
-
-
 		}
-
 
         //Woonjiet comment
 		//If decided point
