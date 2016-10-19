b70ab4d887954e925b580bac1242611fecf95cae
githubflim1 <foongfoong123@yahoo.com>
2016-10-19 16:03:35 +0800

    Location calculation code. First working version. Need optimization still.

--- RedpinAndroid/src/main/java/org/redpin/android/wifi/WifiInformation.java
+++ RedpinAndroid/src/main/java/org/redpin/android/wifi/WifiInformation.java
@@ -63,9 +63,31 @@ public class WifiInformation {
          for (int j = 0; j < currentMeasurementPerLocation.numOfWifiPoints; j++) {
             WifiInfoRow currentWifiRow = currentMeasurementPerLocation.myWifiInfoRowArray[j];
 
+            Log.i("#####FoongFoong#####",
+                    "measurementAccuracyLevel(): baseWifiRow.BSSID: " + baseWifiRow.BSSID
+                            + ", baseWifiRow.SSID: " + baseWifiRow.SSID
+                            + ", baseWifiRow.level: " + baseWifiRow.level);
+            if (currentWifiRow != null) {
+               Log.i("#####FoongFoong#####",
+                       "measurementAccuracyLevel(): currentWifiRow.BSSID: " + currentWifiRow.BSSID
+                               + ", currentWifiRow.SSID: " + currentWifiRow.SSID
+                               + ", currentWifiRow.level: " + currentWifiRow.level);
+            } else {
+               Log.i("#####FoongFoong#####",
+                       "measurementAccuracyLevel(): currentWifiRow == NULL ");
+            }
+
             //bssid match: add ID contribution and signal strength
             if (baseWifiRow != null && baseWifiRow.BSSID != null && currentWifiRow != null && currentWifiRow.BSSID != null
                     && baseWifiRow.BSSID.equals(currentWifiRow.BSSID)) {
+               Log.i("#####FoongFoong#####",
+                       "measurementAccuracyLevel(): MATCH baseWifiRow.BSSID: " + baseWifiRow.BSSID
+                               + ", baseWifiRow.SSID: " + baseWifiRow.SSID
+                               + ", baseWifiRow.level: " + baseWifiRow.level);
+               Log.i("#####FoongFoong#####",
+                       "measurementAccuracyLevel(): MATCH currentWifiRow.BSSID: " + currentWifiRow.BSSID
+                               + ", currentWifiRow.SSID: " + currentWifiRow.SSID
+                               + ", currentWifiRow.level: " + currentWifiRow.level);
                account += ID_POS_CONTRIBUTION;
                account += signalContribution(baseWifiRow.level, currentWifiRow.level);
             }
@@ -85,7 +107,7 @@ public class WifiInformation {
       }
 
       Log.i("#####FoongFoong#####",
-              "measurementSimilarityLevel(): Account: " + account
+              "measurementAccuracyLevel(): Account: " + account
                       + ", Total Credit possible: " + totalCredit
                       + ", Accuracy: " + accuracy);
 
@@ -93,6 +115,40 @@ public class WifiInformation {
    }
 
    public void updateInformation(List<ScanResult> wifiScanList) {
+
+      //----------------------------------------------------------------------------------------------------
+      //   TODO: move this code back to initializeFingerprintDb() later
+      //----------------------------------------------------------------------------------------------------
+      fingerprintDB = new FingerprintDatabase(3);
+
+      fingerprintDB.fillUpEachMeasurementPerLocation(0,"A1",3);
+      fingerprintDB.fillUpEachMeasurementPerLocation(1,"B1",3);
+      fingerprintDB.fillUpEachMeasurementPerLocation(2,"C1",3);
+
+
+      Log.i("wj", "initializeFingerprintDb part 1 complete ");
+
+      fingerprintDB.myMeasurementPerLocationArray[0].fillUpEachWifiInfoRow(0,"84:24:8d:40:84:00","M-Wireless", 45);
+      fingerprintDB.myMeasurementPerLocationArray[0].fillUpEachWifiInfoRow(1,"84:24:8d:40:4d:81","M-Guest", 63);
+      fingerprintDB.myMeasurementPerLocationArray[0].fillUpEachWifiInfoRow(2,"84:24:8d:40:68:91","M-Guest", 65);
+
+      Log.i("wj", "initializeFingerprintDb part 2 complete ");
+
+      fingerprintDB.myMeasurementPerLocationArray[1].fillUpEachWifiInfoRow(0,"84:24:8d:40:2f:11","M-Guest", 61);
+      fingerprintDB.myMeasurementPerLocationArray[1].fillUpEachWifiInfoRow(1,"84:24:8d:3f:be:b1","M-Guest", 59);
+      fingerprintDB.myMeasurementPerLocationArray[1].fillUpEachWifiInfoRow(2,"84:24:8d:3f:e2:51","M-Guest", 43);
+
+      Log.i("wj", "initializeFingerprintDb part 3 complete ");
+
+      fingerprintDB.myMeasurementPerLocationArray[2].fillUpEachWifiInfoRow(0,"84:24:8d:3f:d1:e0","M-Wireless", 74);
+      fingerprintDB.myMeasurementPerLocationArray[2].fillUpEachWifiInfoRow(1,"84:24:8d:40:f8:90","M-Wireless", 74);
+      fingerprintDB.myMeasurementPerLocationArray[2].fillUpEachWifiInfoRow(2,"84:24:8d:40:fe:c1","M-Guest", 60);
+      //----------------------------------------------------------------------------------------------------
+      //   END_OF_TODO: move this code back to initializeFingerprintDb() later
+      //----------------------------------------------------------------------------------------------------
+
+
+      // for debugging , u can use --> db.printFingerprintDb();
 
       currentScanList = wifiScanList;
       int currentScanListSize = currentScanList.size();
@@ -108,21 +164,32 @@ public class WifiInformation {
       //----------------------------------------------------------------------------------------------------
       //   Getting current wifi point and data into MeasurementPerLocation
       //----------------------------------------------------------------------------------------------------
-      MeasurementPerLocation currentMeasurementPerLocation = new MeasurementPerLocation("current",currentScanList.size());
-
+      int count = 0;
       for (i = 0; i < currentScanList.size(); i++) {
-         if (currentScanList.get(i).SSID == "M-Wireless" || currentScanList.get(i).SSID == "M-Guest") {
-            currentMeasurementPerLocation.fillUpEachWifiInfoRow(i, currentScanList.get(i).BSSID, currentScanList.get(i).SSID, Math.abs(currentScanList.get(i).level));
+         if (currentScanList.get(i).SSID.equals("M-Wireless")  || currentScanList.get(i).SSID.equals("M-Guest")) {
+            count++;
          }
       }
+      MeasurementPerLocation currentMeasurementPerLocation = new MeasurementPerLocation("current",count);
+      count = 0;
+      for (i = 0; i < currentScanList.size(); i++) {
+
+         if (currentScanList.get(i).SSID.equals("M-Wireless")  || currentScanList.get(i).SSID.equals("M-Guest")) {
+            currentMeasurementPerLocation.fillUpEachWifiInfoRow(count, currentScanList.get(i).BSSID, currentScanList.get(i).SSID, Math.abs(currentScanList.get(i).level));
+            count++;
+            Log.i("#####FoongFoong#####",
+                    "updateInformation(): MATCH currentScanList.get(i).BSSID: " + currentScanList.get(i).BSSID
+                            + " currentScanList.get(i).SSID: " + currentScanList.get(i).SSID
+                            + " currentScanList.get(i).level: " + currentScanList.get(i).level);
+         }
+      }
+      Log.i("#####FoongFoong#####",
+              "updateInformation(): currentMeasurementPerLocation.numOfWifiPoints: " + currentMeasurementPerLocation.numOfWifiPoints
+               + " count: " + count);
 
       // for logging , u can use --> currentMeasurementPerLocation.printWifiInfoRow();
 
       //-----------------------------------------------------------------------------------------------------
-
-
-
-
 
       //---------------------------------------------------------------
       // Log scan list
@@ -143,9 +210,14 @@ public class WifiInformation {
       // Location Calculation
       //---------------------------------------------------------------
       //Compare measured data with each location's data in database
-      for (int j = 0; j < fingerprintDB.numOfLocations; j++) {
+      for (int j = 0; fingerprintDB != null && j < fingerprintDB.numOfLocations; j++) {
          MeasurementPerLocation baseMeasurementPerLocation = fingerprintDB.myMeasurementPerLocationArray[j];
-         int accuracyLevelPerLocation = measurementAccuracyLevel(baseMeasurementPerLocation, currentMeasurementPerLocation);
+         Log.i("#####FoongFoong#####",
+                 "updateInformation(): FOR LOOP baseMeasurementPerLocation.numOfWifiPoints: " + baseMeasurementPerLocation.numOfWifiPoints);
+         int accuracyLevelPerLocation = 0;
+         if (baseMeasurementPerLocation != null && currentMeasurementPerLocation != null) {
+            accuracyLevelPerLocation = measurementAccuracyLevel(baseMeasurementPerLocation, currentMeasurementPerLocation);
+         }
 
          //Store the highest accuracy level and its MeasurementPerLocation class pointer
          if (accuracyLevelPerLocation > highestAccuracyLevel) {
@@ -162,19 +234,26 @@ public class WifiInformation {
                  "updateInformation(): Highest Accuracy Level Location Name: " + highAccuracyMeasurementPerLocation.name);
 
          //---------------------------------------------------------------
-         // Show Location
+         // TODO: Show Location, temp, change later.
          //---------------------------------------------------------------
          String locationString = highAccuracyMeasurementPerLocation.name;
          if (locationString.equals("A1")) {
             MapViewActivity.setMarkerLocation(205, 8);
+            Log.i("#####FoongFoong#####", "updateInformation(): At A1: ");
          } else if (locationString.equals("B1")) {
             MapViewActivity.setMarkerLocation(297, 8);
+            Log.i("#####FoongFoong#####", "updateInformation(): At B1: ");
          } else if (locationString.equals("C1")) {
             MapViewActivity.setMarkerLocation(599, 50);
+            Log.i("#####FoongFoong#####", "updateInformation(): At C1: ");
          } else {
-            //to prove that it not work, point to E1
-            MapViewActivity.setMarkerLocation(167, 45);
+            //to prove that it not work, point to E11
+            MapViewActivity.setMarkerLocation(167, 412);
+            Log.i("#####FoongFoong#####", "updateInformation(): NOT MATCH... At E11: ");
          }
+         //---------------------------------------------------------------
+         // END_OF_TODO: Show Location, temp, change later.
+         //---------------------------------------------------------------
       }
    }
 
@@ -184,33 +263,7 @@ public class WifiInformation {
       // Initialize fingerprint database , store all hard-coded database information during this function
       //------------------------------------------------------------------------------------------------
 
-      db = new FingerprintDatabase(3);
 
-      db.fillUpEachMeasurementPerLocation(0,"A1",3);
-      db.fillUpEachMeasurementPerLocation(1,"B1",3);
-      db.fillUpEachMeasurementPerLocation(2,"C1",3);
-
-
-      Log.i("wj", "initializeFingerprintDb part 1 complete ");
-
-      db.myMeasurementPerLocationArray[0].fillUpEachWifiInfoRow(0,"asdasdasdas","M-guest", 56);
-      db.myMeasurementPerLocationArray[0].fillUpEachWifiInfoRow(1,"asdasdasdas","M-guest", 56);
-      db.myMeasurementPerLocationArray[0].fillUpEachWifiInfoRow(2,"asdasdasdas","M-guest", 56);
-
-      Log.i("wj", "initializeFingerprintDb part 2 complete ");
-
-      db.myMeasurementPerLocationArray[1].fillUpEachWifiInfoRow(0,"asdasdasdas","M-guest", 56);
-      db.myMeasurementPerLocationArray[1].fillUpEachWifiInfoRow(1,"asdasdasdas","M-guest", 56);
-      db.myMeasurementPerLocationArray[1].fillUpEachWifiInfoRow(2,"asdasdasdas","M-guest", 56);
-
-      Log.i("wj", "initializeFingerprintDb part 3 complete ");
-
-      db.myMeasurementPerLocationArray[2].fillUpEachWifiInfoRow(0,"asdasdasdas","M-guest", 56);
-      db.myMeasurementPerLocationArray[2].fillUpEachWifiInfoRow(1,"asdasdasdas","M-guest", 56);
-      db.myMeasurementPerLocationArray[2].fillUpEachWifiInfoRow(2,"asdasdasdas","M-guest", 56);
-
-
-      // for debugging , u can use --> db.printFingerprintDb();
    }
 
    public String[] getStringArray()
